4f67c0987232a893003c09dca5c6f52ea567562a
Kagaya Manami <ys.traning230715@gmail.com>
2025-08-20 15:11:27 +0900

    styled-system追加

---
+++ styled-system/panda-safelist.ts
@@ -0,0 +1,12 @@
+
+/**
+ * Auto-generated safelist for Panda CSS
+ */
+export const safelist = [
+  "button--variant_successFilled",
+  "button--variant_successText",
+  "button--variant_dangerFilled",
+  "button--variant_dangerText",
+  "button--variant_neutralFilled",
+  "button--variant_neutralText"
+];
---
+++ styled-system/recipes/button-recipe.d.ts
@@ -0,0 +1,35 @@
+/* eslint-disable */
+import type { ConditionalValue } from '../types/index';
+import type { DistributiveOmit, Pretty } from '../types/system-types';
+
+interface ButtonRecipeVariant {
+  /**
+ * @default "filled"
+ */
+variant: "filled" | "subtle"
+/**
+ * @default "success"
+ */
+intent: "success" | "danger" | "neutral"
+}
+
+type ButtonRecipeVariantMap = {
+  [key in keyof ButtonRecipeVariant]: Array<ButtonRecipeVariant[key]>
+}
+
+export type ButtonRecipeVariantProps = {
+  [key in keyof ButtonRecipeVariant]?: ConditionalValue<ButtonRecipeVariant[key]> | undefined
+}
+
+export interface ButtonRecipeRecipe {
+  __type: ButtonRecipeVariantProps
+  (props?: ButtonRecipeVariantProps): string
+  raw: (props?: ButtonRecipeVariantProps) => ButtonRecipeVariantProps
+  variantMap: ButtonRecipeVariantMap
+  variantKeys: Array<keyof ButtonRecipeVariant>
+  splitVariantProps<Props extends ButtonRecipeVariantProps>(props: Props): [ButtonRecipeVariantProps, Pretty<DistributiveOmit<Props, keyof ButtonRecipeVariantProps>>]
+  getVariantProps: (props?: ButtonRecipeVariantProps) => ButtonRecipeVariantProps
+}
+
+
+export declare const buttonRecipe: ButtonRecipeRecipe
